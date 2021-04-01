16d85195494dda806ef0f859fe3024ef06c18264
Vladislav Legavin <vldikk12@gmail.com>
2021-04-01 22:47:03 +0300

    first commit

---
+++ app/src/main/java/com/example/phonestorebless/personal/bookKeep/BookKeepingActivity.kt
@@ -0,0 +1,11 @@
+package com.example.phonestorebless
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class BookKeepingActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_book_keeping)
+    }
+}
